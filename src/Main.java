016c8ea7028ab7413f77466e8935186943266b30
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-17 20:27:37 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(first commit);
+
+        for (int i=1; i <=10; i=i+1) {
+            System.out.println("Числа от 1 до 10: " + i);
+        }
     }
 }
