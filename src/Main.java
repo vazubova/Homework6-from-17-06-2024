618ac4b5bce4606f845151c3161e0ea16d9877ff
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-17 21:10:29 +0300

    Девятое задание

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,51 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i=1; i <=10; i=i+1) {
+        for (int i = 1; i <= 10; i = i + 1) {
             System.out.println("Числа от 1 до 10: " + i);
         }
 
-        for (int f=10; f>=1; f=f-1) {
+        for (int f = 10; f >= 1; f = f - 1) {
             System.out.println("Числа от 10 до 1: " + f);
         }
 
-        for (int s = 0; s<=17 && s>=0; s=s+2) {
+        for (int s = 0; s <= 17 && s >= 0; s = s + 2) {
             System.out.println("Чётные числа от 0 до 17: " + s);
         }
-        for (int d = 10; d<=10 && d>=-10; d=d-1) {
+        for (int d = 10; d <= 10 && d >= -10; d = d - 1) {
             System.out.println("Числа от 10 до -10: " + d);
         }
 
-        for (int h = 1904; h<=2096; h=h+4) {
+        for (int h = 1904; h <= 2096; h = h + 4) {
             System.out.println(h + " год является високосным");
         }
 
-        for (int k=7; k<=98; k=k+7) {
+        for (int k = 7; k <= 98; k = k + 7) {
             System.out.println("Все числа последовательности от 7 до 98 с разницей +7: " + k);
         }
 
-        for (int j = 1; j<=512; j=j*2) {
+        for (int j = 1; j <= 512; j = j * 2) {
             System.out.println("Последовательность чисел от 1 до 512 с умножением на 2: " + j);
         }
 
         int whole = 0;
         int start = 29000;
-        for (int m=1; m<=12; m++) {
+        for (int m = 1; m <= 12; m++) {
 
             whole = whole + start;
             System.out.println("Месяц " + m + " сумма накоплений равна" + whole + " рублей");
 
         }
 
+        int inBank = 0;
+        int first = 29000;
+        for (int t = 1; t <= 12; t++) {
 
+            inBank = inBank + inBank / 100;
+            inBank = inBank + first;
+            System.out.println("Месяц " + t + " сумма накоплений равна" + inBank + " рублей");
+
+
+        }
     }
 }
