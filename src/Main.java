9319c58a7cfae93dbd87e827f2efbfddbb16b871
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-17 20:29:55 +0300

    Второе задание

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         for (int i=1; i <=10; i=i+1) {
             System.out.println("Числа от 1 до 10: " + i);
         }
+
+        for (int f=10; f>=1; f=f-1) {
+            System.out.println("Числа от 10 до 1: " + f);
+        }
     }
 }
